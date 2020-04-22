f9f37def4e716e560c48ccf5aa5abd9aa151e770
asmoffet <54548893+asmoffet@users.noreply.github.com>
2020-04-22 12:39:57 -0500

    searching and filtering done and working!

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -10,9 +10,53 @@ namespace Movies.Pages
     public class IndexModel : PageModel
     {
 
-        public void OnGet()
-        {
+        /// <summary>
+        /// The movies to display on the index page
+        /// </summary>
+        public IEnumerable<Movie> Movies { get; protected set; }
 
+        /// <summary>
+        /// The current search terms 
+        /// </summary>
+        [BindProperty]
+        public string SearchTerms { get; set; } = "";
+
+        /// <summary>
+        /// The filtered MPAA Ratings
+        /// </summary>
+        [BindProperty]
+        public string[] MPAARatings { get; set; }
+
+        /// <summary>
+        /// The filtered genres
+        /// </summary>
+        [BindProperty]
+        public string[] Genres { get; set; }
+
+        /// <summary>
+        /// The minimum IMDB Rating
+        /// </summary>
+        [BindProperty]
+        public double? IMDBMin { get; set; }
+
+        /// <summary>
+        /// The maximum IMDB Rating
+        /// </summary>
+        [BindProperty]
+        public double? IMDBMax { get; set; }
+        /// <summary>
+        /// Gets the search results for display on the page
+        /// </summary>
+        public void OnGet(double? IMDBMin, double? IMDBMax)
+        {
+            // Nullable conversion workaround
+            this.IMDBMin = IMDBMin;
+            this.IMDBMax = IMDBMax;
+            Movies = MovieDatabase.Search(SearchTerms);
+            Movies = MovieDatabase.FilterByMPAARating(Movies, MPAARatings);
+            Movies = MovieDatabase.FilterByGenre(Movies, Genres);
+            Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
         }
+
     }
 }
